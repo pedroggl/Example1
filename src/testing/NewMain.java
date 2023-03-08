a47d6f357360334ec2c92b13b7559958469ff18c
pedroggl <gallardolozanopedrogabriel@gmail.com>
2023-03-08 11:40:25 +0100

    Mi primer commit

---
+++ src/testing/NewMain.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        String matricula = "AS";
+        System.out.println(matricula.matches("[A-Z]{3,10}"));
+    }
+
+}
---
+++ src/testing/Testing.java
@@ -0,0 +1,24 @@
+package testing;
+
+import java.util.Scanner;
+
+public class Testing {
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int precio[] = new int[5];
+        for (int i = 0; i < precio.length; i++) {
+            System.out.println("Introduzca un numero");
+            precio[i] = in.nextInt();
+            System.out.println("Precio con descuento es: " + precioConIVA(precio[i]));
+        }
+    }
+
+    public static double precioConIVA(double precio) {
+        double IVA = precio * 21 / 100;
+        double precioConIVA = precio + IVA;
+        return precioConIVA;
+    }
+
+}
